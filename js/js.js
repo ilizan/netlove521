d08bf20d37a0e3d6490ba7b5645925890e61d954
lizan <lizan@shengbangtec.com>
2020-07-31 16:04:42 +0800

    tongbu

--- js/js.js
+++ js/js.js
@@ -1,50 +1,67 @@
-$(function () {
-    /*var config = {
-
+function timeElapse(date){
+    var current = Date();
+    var seconds = (Date.parse(current) - Date.parse(date)) / 1000;
+    var days = Math.floor(seconds / (3600 * 24));
+    seconds = seconds % (3600 * 24);
+    var hours = Math.floor(seconds / 3600);
+    if (hours < 10) {
+        hours = "0" + hours;
     }
-    CanvasParticle(config);*/
-
-    function timeElapse(date){
-        var current = Date();
-        var seconds = (Date.parse(current) - Date.parse(date)) / 1000;
-        var days = Math.floor(seconds / (3600 * 24));
-        seconds = seconds % (3600 * 24);
-        var hours = Math.floor(seconds / 3600);
-        if (hours < 10) {
-            hours = "0" + hours;
-        }
-        seconds = seconds % 3600;
-        var minutes = Math.floor(seconds / 60);
-        if (minutes < 10) {
-            minutes = "0" + minutes;
-        }
-        seconds = seconds % 60;
-        if (seconds < 10) {
-            seconds = "0" + seconds;
-        }
-        var result = "第 <span class=\"digit\">" + days + "</span> 天 <br/><span class=\"digit\">" + hours + "</span> 小时 <span class=\"digit\">" + minutes + "</span> 分钟 <span class=\"digit\">" + seconds + "</span> 秒";
-        $("#clock").html(result);
+    seconds = seconds % 3600;
+    var minutes = Math.floor(seconds / 60);
+    if (minutes < 10) {
+        minutes = "0" + minutes;
     }
-
-    function s(){
-        var together = new Date();
-        together.setFullYear(2012, 12, 31);
-        together.setHours(21);
-        together.setMinutes(04);
-        together.setSeconds(00);
-        together.setMilliseconds(0);
-        timeElapse(together)
+    seconds = seconds % 60;
+    if (seconds < 10) {
+        seconds = "0" + seconds;
     }
+    var result = "第 <span class=\"digit\">" + days + "</span> 天 <br/><span class=\"digit\">" + hours + "</span> 小时 <span class=\"digit\">" + minutes + "</span> 分钟 <span class=\"digit\">" + seconds + "</span> 秒";
+    //$("#clock").html(result);
+    document.getElementById('clock').innerHTML=result
+}
+
+function s(){
+    var together = new Date();
+    together.setFullYear(2012, 12, 31);
+    together.setHours(21);
+    together.setMinutes(04);
+    together.setSeconds(00);
+    together.setMilliseconds(0);
+    timeElapse(together)
+}
+
+
+window.onload=function(){
+    new Typed("#sText", {
+        strings: ['一个人，一座城，一世心疼。<br/>一个人，一支伞，一世等待。'],
+        startDelay: 1000,//开始前延迟时间
+        typeSpeed: 100,//数值越大 速度越慢
+        loop: true,
+        backSpeed: 50,
+        showCursor: true
+    });
+    const sakura = new Sakura('body', {
+        colors: [
+            {
+                gradientColorStart: 'rgba(255, 183, 197, 0.9)',
+                gradientColorEnd: 'rgba(255, 197, 208, 0.9)',
+                gradientColorDegree: 10,
+            },
+            // {
+            //     gradientColorStart: 'rgba(255,189,189)',
+            //     gradientColorEnd: 'rgba(227,170,181)',
+            //     gradientColorDegree: 10,
+            // },
+            // {
+            //     gradientColorStart: 'rgba(212,152,163)',
+            //     gradientColorEnd: 'rgba(242,185,196)',
+            //     gradientColorDegree: 10,
+            // },
+        ],
+    });
     s();
     setInterval(function(){
         s()
     },1000);
-    $('body').on('mousemove', function(e) {
-        var offsetX = e.clientX / window.innerWidth - 0.5,
-            offsetY = e.clientY / window.innerHeight - 0.5;
-        var _left = -40 * offsetX;    //如果想动的幅度更大，可以调整 -40 的值
-        var _top = -40 * offsetY;     //如果想动的幅度更大，可以调整 -40 的值
-        //应用公式
-        $('.content').css('left',_left*1.2).css('top',_top*0.2);  //将您的left值和top值先+此数值，*的小数越大，动的越大，否则越小
-    });
-})
+}
